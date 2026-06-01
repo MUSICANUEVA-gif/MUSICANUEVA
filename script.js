70924d44f17a580caf3cb637b8d903bf7f1b0dc3
PSICOSIS <musicanueva.promo@gmail.com>
2026-05-31 20:08:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
 
+const player =
+document.getElementById("audioPlayer");
+
+const titulo =
+document.getElementById("titulo");
+
+const cover =
+document.getElementById("cover");
+
+const downloadBtn =
+document.getElementById("downloadBtn");
+
+function cargarCancion(){
+
+const url =
+document.getElementById("urlInput").value;
+
+player.src = url;
+
+titulo.innerText =
+"Cargando...";
+
+jsmediatags.read(url, {
+
+onSuccess: function(tag){
+
+titulo.innerText =
+tag.tags.title || "Sin título";
+
+if(tag.tags.picture){
+
+let data =
+tag.tags.picture.data;
+
+let format =
+tag.tags.picture.format;
+
+let byteArray =
+new Uint8Array(data);
+
+let blob =
+new Blob([byteArray],
+{type: format});
+
+let imageUrl =
+URL.createObjectURL(blob);
+
+cover.src = imageUrl;
+}
+
+},
+
+onError: function(error){
+
+titulo.innerText =
+"Archivo cargado";
+
+console.log(error);
+
+}
+
+});
+
+downloadBtn.onclick = function(){
+
+const a =
+document.createElement("a");
+
+a.href = url;
+
+a.download = "";
+
+document.body.appendChild(a);
+
+a.click();
+
+document.body.removeChild(a);
+
+};
+
+}
