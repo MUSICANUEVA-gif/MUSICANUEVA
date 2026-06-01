c6d644962aebf071628f428819e7fac78d078a8a
PSICOSIS <musicanueva.promo@gmail.com>
2026-05-31 20:58:24 -0500

    Delete get_albums.php

--- get_albums.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once '../config.php';
-
-$results = $db->query("SELECT * FROM albums");
-$albums = $results->fetchAll(PDO::FETCH_ASSOC);
-
-echo json_encode($albums);
-?>
