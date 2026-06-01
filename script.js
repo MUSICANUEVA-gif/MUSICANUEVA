fdd2c91c49cab7cf7a8be960ab091d005e908653
PSICOSIS <musicanueva.promo@gmail.com>
2026-05-31 20:32:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-const player =
-document.getElementById("audioPlayer");
+import {
+  getDatabase,
+  ref,
+  push,
+  get,
+  remove
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-const titulo =
-document.getElementById("titulo");
-
-const cover =
-document.getElementById("cover");
-
-const downloadBtn =
-document.getElementById("downloadBtn");
-
-function cargarCancion(){
-
-const url =
-document.getElementById("urlInput").value;
-
-player.src = url;
-
-titulo.innerText =
-"Cargando...";
-
-jsmediatags.read(url, {
-
-onSuccess: function(tag){
-
-titulo.innerText =
-tag.tags.title || "Sin título";
-
-if(tag.tags.picture){
-
-let data =
-tag.tags.picture.data;
-
-let format =
-tag.tags.picture.format;
-
-let byteArray =
-new Uint8Array(data);
-
-let blob =
-new Blob([byteArray],
-{type: format});
-
-let imageUrl =
-URL.createObjectURL(blob);
-
-cover.src = imageUrl;
-}
-
-},
-
-onError: function(error){
-
-titulo.innerText =
-"Archivo cargado";
-
-console.log(error);
-
-}
-
-});
-
-downloadBtn.onclick = function(){
-
-const a =
-document.createElement("a");
-
-a.href = url;
-
-a.download = "";
-
-document.body.appendChild(a);
-
-a.click();
-
-document.body.removeChild(a);
-
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "psicosismusic-2b04a.firebaseapp.com",
+  databaseURL: "https://psicosismusic-2b04a-default-rtdb.firebaseio.com",
+  projectId: "psicosismusic-2b04a",
+  storageBucket: "psicosismusic-2b04a.firebasestorage.app",
+  messagingSenderId: "233441166744",
+  appId: "1:233441166744:web:6757b5fb8d4fd1803697ed"
 };
 
-}
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
+export { ref, push, get, remove };
